cd48bc42cb02cf7f93506151604304d6a507c005
thiago lopes de mello <timabuntu@gmail.com>
2022-06-18 16:09:18 -0300

    feat: get  lessons data query

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
+import { gql } from "@apollo/client";
+import { useEffect } from "react";
+import { client } from "./lib/apollo";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
 function App() {
+  useEffect(() => {
+    client
+      .query({
+        query: GET_LESSONS_QUERY,
+      })
+      .then((response) => console.log(response.data));
+  }, []);
+
   return (
     <div className="App">
       <h1>Study Schedule</h1>
