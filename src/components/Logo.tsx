5f67cc08fe80d250547c8ee2905c67adf1d15427
thiago lopes de mello <timabuntu@gmail.com>
2022-06-20 13:08:34 -0300

    chore: changed  font height

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,8 @@
 export function Logo() {
   return (
     <h1 className="flex items-center gap-1 uppercase text-zinc-300">
-      <strong className="text-2xl normal-case text-zinc-50">Study</strong>
-      <span className="mb-1 text-3xl font-black text-blue-500">|</span>Schedule
+      <strong className="mb-1 text-3xl normal-case text-zinc-50">Study</strong>
+      <span className="mb-1 text-4xl font-black text-blue-500">|</span>Schedule
     </h1>
   );
 }
