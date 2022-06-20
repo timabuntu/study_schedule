f4ec6c08be6938f51da3768c4638ba26e8d9da36
thiago lopes de mello <timabuntu@gmail.com>
2022-06-20 13:11:10 -0300

    styled: styles header created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
 import { Logo } from "./Logo";
 
 export function Header() {
-  return <Logo />;
+  return (
+    <header className="flex justify-center py-5 bg-gray-700 border-b border-gray-600">
+      <Logo />
+    </header>
+  );
 }
