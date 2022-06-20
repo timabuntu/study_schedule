73fd4f1428e80030d400ad7bfb1d0c9d8ae40852
thiago lopes de mello <timabuntu@gmail.com>
2022-06-20 12:21:57 -0300

    style: styles applied to the component logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,8 @@
 export function Logo() {
   return (
-    <h1>
-      <strong>Study </strong>
-      <span>|</span> Schedule
+    <h1 className="flex items-center gap-1 uppercase text-zinc-300">
+      <strong className="text-2xl normal-case text-zinc-50">Study</strong>
+      <span className="mb-1 text-3xl font-black text-blue-500">|</span>Schedule
     </h1>
   );
 }
