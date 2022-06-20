3af88bd23acd8d1985e83468eea4928dac037e06
thiago lopes de mello <timabuntu@gmail.com>
2022-06-20 12:35:00 -0300

    style: defined colors tailwind palette for theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        colors: {
+          green: {
+            500: "##00875F",
+          },
+          orange: {
+            500: "#FBA94C",
+          },
+          red: {
+            500: "#F75A68",
+          },
+          gray: {
+            100: "#E1E1E6",
+            200: "#C4C4CC",
+            300: "#8D8D99",
+            500: "#323238",
+            600: "#29292E",
+            700: "#121214",
+            900: "#09090A",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
