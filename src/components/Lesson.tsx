d32d48dec6c125015cf2e63ac51754ca38282b02
thiago lopes de mello <timabuntu@gmail.com>
2022-06-24 10:34:41 -0300

    style: sidebar elements

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,16 +1,20 @@
 export function Lesson() {
   return (
     <a href="#">
-      <span>Terça . 22 de junho . 19h00</span>
+      <span className="text-gray-300">Terça · 22 de junho · 19h00</span>
 
-      <div>
-        <header>
-          <span>Conteúdo liberado</span>
-          <span>CHEGA MAIS</span>
+      <section className="p-4 mt-2 border border-gray-500 rounded">
+        <header className="flex items-center justify-between">
+          <span className="text-sm font-medium text-green-500">
+            Conteúdo liberado
+          </span>
+          <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-blue-300 font-bold">
+            CHEGA AÍ
+          </span>
         </header>
 
-        <strong>Docker é Vida</strong>
-      </div>
+        <strong className="block mt-5 text-gray-200">Docker é Vida</strong>
+      </section>
     </a>
   );
 }
