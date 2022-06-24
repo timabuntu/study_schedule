610fb7b3d47903863b99edb59b79a35262493358
thiago lopes de mello <timabuntu@gmail.com>
2022-06-24 09:15:11 -0300

    chore: Lesson component imported

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,19 @@
+import { Lesson } from "./Lesson";
+
 export function Sidebar() {
-  return <h1>Sidebar</h1>;
+  return (
+    <aside className="w-[348px] bg-gray-700 p-6 border-r border-gray-600">
+      <span className="block pb-6 text-2xl font-bold border-b border-gray-500 ">
+        Cronograma das aulas
+      </span>
+
+      <div>
+        <Lesson />
+        <Lesson />
+        <Lesson />
+        <Lesson />
+        <Lesson />
+      </div>
+    </aside>
+  );
 }
