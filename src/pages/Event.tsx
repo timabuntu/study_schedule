257a02d29864054a39e08a5e06ec384908d22474
thiago lopes de mello <timabuntu@gmail.com>
2022-06-24 08:41:40 -0300

    chore: sidebar component imported

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,5 +1,13 @@
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export function Event() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Sidebar />
+      </main>
+    </div>
+  );
 }
