d6cb3b3bb583f96f0b86a1a07132f277a8a2efba
thiago lopes de mello <timabuntu@gmail.com>
2022-06-25 13:39:52 -0300

    chore: fake props

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,12 +8,12 @@ export function Sidebar() {
       </span>
 
       <section className="flex flex-col gap-8 pt-8">
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
+        <Lesson
+          title="Aula 01"
+          slug="aula-01"
+          availableAt={new Date()}
+          type="class"
+        />
       </section>
     </aside>
   );
