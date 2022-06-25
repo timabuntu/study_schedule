b1ec5760a1ec3f31945774cb35b2457e4aa035e4
thiago lopes de mello <timabuntu@gmail.com>
2022-06-25 13:30:10 -0300

    fix: interface LessonProps ajustada

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -4,13 +4,13 @@ interface LessonProps {
   title: string;
   slug: string;
   availableAt: Date;
-  type: "live | class";
+  type: "live" | "class";
 }
 
 export function Lesson(props: LessonProps) {
   return (
     <a href="#">
-      <span className="text-gray-300">Terça · 22 de junho · 19h00</span>
+      <span className="text-gray-300">{props.availableAt.toString()}</span>
 
       <section className="p-4 mt-2 border border-gray-500 rounded">
         <header className="flex items-center justify-between">
@@ -19,7 +19,7 @@ export function Lesson(props: LessonProps) {
             Conteúdo liberado
           </span>
           <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-blue-300 font-bold">
-            CHEGA AÍ
+            {props.type === "live" ? "CHEGA AÍ" : "AULA PRÁTICA"}
           </span>
         </header>
 
