399ce8eca3b98582489dbccd30aaf183a3f520c3
thiago lopes de mello <timabuntu@gmail.com>
2022-06-25 13:44:37 -0300

    feat: condition if lesson available

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,4 @@
-import { CheckCircle } from "phosphor-react";
+import { CheckCircle, Lock } from "phosphor-react";
 
 interface LessonProps {
   title: string;
@@ -16,10 +16,17 @@ export function Lesson(props: LessonProps) {
 
       <section className="p-4 mt-2 border border-gray-500 rounded">
         <header className="flex items-center justify-between">
-          <span className="flex items-center gap-2 text-sm font-medium text-green-500">
-            <CheckCircle size={20} />
-            Conteúdo liberado
-          </span>
+          {isLessonAvailable ? (
+            <span className="flex items-center gap-2 text-sm font-medium text-green-500">
+              <CheckCircle size={20} />
+              Conteúdo liberado
+            </span>
+          ) : (
+            <span className="flex items-center gap-2 text-sm font-medium text-orange-500">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
           <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-blue-300 font-bold">
             {props.type === "live" ? "CHEGA AÍ" : "AULA PRÁTICA"}
           </span>
