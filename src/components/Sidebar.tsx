49306bc990872c28d0397156a602e2f2b76e0cb0
thiago lopes de mello <timabuntu@gmail.com>
2022-06-25 13:52:40 -0300

    feat: fetch  lesson graphcms

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,22 @@
+import { gql, useQuery } from "@apollo/client";
 import { Lesson } from "./Lesson";
 
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      availableAt
+      title
+      slug
+    }
+  }
+`;
+
 export function Sidebar() {
+  const { data } = useQuery(GET_LESSONS_QUERY);
+  console.log(data);
+
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-r border-gray-600">
       <span className="block pb-6 text-2xl font-bold border-b border-gray-500 ">
