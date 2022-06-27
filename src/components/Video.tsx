20b522dc759bb112aa3e2f3ea8cfb8ab85dcc5c6
thiago lopes de mello <timabuntu@gmail.com>
2022-06-27 12:32:05 -0300

    style: styled elements

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -3,34 +3,51 @@ import { ControlVideo } from './ControlVideo';
 export function Video() {
   return (
     <div className='flex-1'>
-      <section className='bg-black'>
+      <section className='flex justify-center bg-black'>
         <main className='h-full w-full max-w-[1100px] max-h-[60vh] aspect-video'>
           <ControlVideo />
         </main>
       </section>
-      <section className='p-8 mx-auto bg-blue-600 max-w-[1100px]'>
+      <section className='p-8 mx-auto  max-w-[1100px]'>
         <header className='flex items-start gap-16'>
-          <div className='bg-pink-500'>
-            <h1>Aula 01 - Início do Estudo de Docker</h1>
-            <p>
+          <div className='flex-1'>
+            <h1 className='text-2xl font-bold'>
+              Aula 01 - Início do Estudo de Docker
+            </h1>
+            <p className='mt-4 leading-relaxed text-gray-200'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
               rerum accusantium est tempore soluta eligendi, laboriosam ut
               dolorem quod quis, illum cum, porro quam. Culpa delectus nesciunt
               qui est autem.
             </p>
-          </div>
-
-          <div className='bg-green-500'>
-            <img src='https://github.com/timabuntu.png' alt='' />
-            <div>
-              <strong>Thiago lopes de Mello</strong>
-              <span>Web Developer</span>
+            <div className='flex items-center gap-4 mt-6'>
+              <img
+                className='w-16 h-16 border-2 border-blue-500 rounded-full'
+                src='https://github.com/timabuntu.png'
+                alt='foto do Instrutor'
+              />
+              <div>
+                <strong className='block text-2xl font-bold'>
+                  Thiago lopes de Mello
+                </strong>
+                <span className='text-sm text-gray-200'>Web Developer</span>
+              </div>
             </div>
           </div>
 
-          <div>
-            <a href='#'>Faça sua Anotação</a>
-            <a href='#'></a>
+          <div className='flex flex-col gap-4'>
+            <a
+              href='#'
+              className='px-6 py-4 text-sm font-bold text-gray-700 uppercase transition-colors bg-yellow-200 rounded hover:bg-yellow-300 hover:text-black'
+            >
+              Faça sua Anotação
+            </a>
+            <a
+              href='#'
+              className='px-6 py-4 text-sm font-bold text-yellow-200 uppercase border border-yellow-200 rounded'
+            >
+              Web Developer
+            </a>
           </div>
         </header>
       </section>
