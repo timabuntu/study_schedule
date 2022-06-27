9e99ce70c66a4b6a6606964dd3b569407fdcd763
thiago lopes de mello <timabuntu@gmail.com>
2022-06-27 09:46:49 -0300

    chore: div element renamed

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,7 @@
 export function Video() {
   return (
     <div className='flex-1'>
-      <div className='bg-black'>Control Player</div>
+      <section className='bg-black'>Control Player</section>
       <section className='p-8 mx-auto bg-blue-600 max-w-[1100px]'>
         <header className='flex items-start gap-16'>
           <div className='bg-pink-500'>
