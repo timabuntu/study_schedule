babdd50b7e807a59de8870c8665fcb59b726e61b
thiago lopes de mello <timabuntu@gmail.com>
2022-06-27 09:36:51 -0300

    style: new separation color

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,8 @@ export function Logo() {
   return (
     <h1 className='flex items-center gap-1 uppercase text-zinc-300'>
       <strong className='mb-1 text-3xl normal-case text-zinc-50'>Study</strong>
-      <span className='mb-1 text-4xl font-black text-blue-500'>|</span>Schedule
+      <span className='mb-1 text-4xl font-black text-yellow-200'>|</span>
+      Schedule
     </h1>
   );
 }
