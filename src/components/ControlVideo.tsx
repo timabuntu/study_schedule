ec69122ddae93b1635e5da8240fcfb7b8e1072af
thiago lopes de mello <timabuntu@gmail.com>
2022-06-27 10:03:11 -0300

    chore: control video style moved to div main

--- src/components/ControlVideo.tsx
+++ src/components/ControlVideo.tsx
@@ -1,7 +1,3 @@
 export function ControlVideo() {
-  return (
-    <main className='h-full w-full max-w-[1100px] max-h-[60vh] aspect-video'>
-      Control Player
-    </main>
-  );
+  return <main>Control Player</main>;
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -4,7 +4,7 @@ export function Video() {
   return (
     <div className='flex-1'>
       <section className='bg-black'>
-        <main>
+        <main className='h-full w-full max-w-[1100px] max-h-[60vh] aspect-video'>
           <ControlVideo />
         </main>
       </section>
