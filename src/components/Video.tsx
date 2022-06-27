ce0a6a13144925e17c20755fe09b3a43eb224b7c
thiago lopes de mello <timabuntu@gmail.com>
2022-06-27 09:19:10 -0300

    style: added div section width

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -2,6 +2,6 @@ export function Video() {
   return (
     <div className='flex-1'>
       <div className='bg-black'>Control Player</div>
-    </div>
+      <section className='p-8 mx-auto bg-blue-600 max-w-[1100px]'>
   );
 }
