fd6ce310a7a207cddcee7c8a2ffc7b1e07e86694
thiago lopes de mello <timabuntu@gmail.com>
2022-06-28 10:16:08 -0300

    chore: imported BrouseRouter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
 import { client } from './lib/apollo';
 import { Router } from './Router';
 
@@ -6,7 +7,9 @@ function App() {
   return (
     <div className='App'>
       <ApolloProvider client={client}>
-        <Router />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
       </ApolloProvider>
     </div>
   );
