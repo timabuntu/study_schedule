786e1a8bcf7cd7abb0140c0b4d8dd42d7402c5cf
thiago lopes de mello <timabuntu@gmail.com>
2022-06-28 10:07:34 -0300

    chore: imported Event component and added path

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
+import { Event } from './pages/Event';
 
 export function Router() {
   return (
     <Routes>
-      <Route />
+      <Route path='/event' element={<Event />} />
     </Routes>
   );
 }
