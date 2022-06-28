483e8a1446792f87019c53fc6183a14aea490f1a
thiago lopes de mello <timabuntu@gmail.com>
2022-06-28 10:09:34 -0300

    chore: removed ApolloProvider component

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -7,8 +6,6 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 );
