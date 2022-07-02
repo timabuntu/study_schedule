939f72f08bc3bdd38090f3ad0a026fcf19c19264
thiago lopes de mello <timabuntu@gmail.com>
2022-07-02 15:55:28 -0300

    feat: added logo

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,7 +1,13 @@
+import { Logo } from '../components/Logo';
+
 export function Subscribe() {
   return (
     <div className='flex flex-col items-center min-h-screen bg-no-repeat bg-cover bg-bullseye'>
-      <main className='max-w-[1100px] flex items-center mx-auto mt-20 justify-between'></main>
+      <main className='max-w-[1100px] flex items-center mx-auto mt-20 justify-between'>
+        <div className='max-w-[640px] '>
+          <Logo />
+        </div>
+      </main>
     </div>
   );
 }
