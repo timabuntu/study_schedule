77ccd5786c8831b10f43d82a0460d8f6ad383de8
thiago lopes de mello <timabuntu@gmail.com>
2022-07-02 15:53:14 -0300

    chore: added main element

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,5 +1,7 @@
 export function Subscribe() {
   return (
-    <div className='flex flex-col items-center min-h-screen bg-no-repeat bg-cover bg-bullseye'></div>
+    <div className='flex flex-col items-center min-h-screen bg-no-repeat bg-cover bg-bullseye'>
+      <main className='max-w-[1100px] flex items-center mx-auto mt-20 justify-between'></main>
+    </div>
   );
 }
