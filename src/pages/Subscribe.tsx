210b648725630163712e629dbf5dfaaa8ddf6dde
thiago lopes de mello <timabuntu@gmail.com>
2022-07-04 09:21:41 -0300

    feat: added title

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -6,6 +6,14 @@ export function Subscribe() {
       <main className='max-w-[1100px] flex items-center mx-auto mt-20 justify-between'>
         <div className='max-w-[640px] '>
           <Logo />
+          <h1 className='mt-8 text-[2.5rem] leading-tight'>
+            Agende seu estudo, e suas{' '}
+            <strong className='text-blue-500'>aulas</strong> ficará{' '}
+            <strong className='text-blue-500'>organizada</strong>
+          </h1>
+          <p className='mt-4 leading-relaxed text-gray-200 '>
+            Apenas a prática levará você para o próximo nível
+          </p>
         </div>
       </main>
       <img
