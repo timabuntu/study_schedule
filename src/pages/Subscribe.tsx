228586226d98378c6b2a67a2958321b9219c7be9
thiago lopes de mello <timabuntu@gmail.com>
2022-07-06 15:53:50 -0300

    feat: created mutation subscribe

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,14 +1,30 @@
+import { gql, useMutation } from '@apollo/client';
 import { useState, FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Logo } from '../components/Logo';
+
+const CREATE_SUBSCRIBER_MUTATION = gql`
+  mutation CreateSubscriber($name: String!, $email: String!) {
+    createSubscriber(data: { name: $name, email: $email }) {
+      id
+    }
+  }
+`;
 
 export function Subscribe() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
+  const [createSubscriber] = useMutation(CREATE_SUBSCRIBER_MUTATION);
+
   function handleSubscribe(event: FormEvent) {
     event.preventDefault();
-
-    console.log(name, email);
+    createSubscriber({
+      variables: {
+        name,
+        email,
+      },
+    });
   }
 
   return (
