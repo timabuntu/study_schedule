ed03edb03248119d92999b08572e7e673e377535
thiago lopes de mello <timabuntu@gmail.com>
2022-07-07 10:35:05 -0300

    chore: classnanes imported

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -2,6 +2,7 @@ import { CheckCircle, Lock } from 'phosphor-react';
 import { isPast, format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { Link, useParams } from 'react-router-dom';
+import classnames from 'classnames';
 interface LessonProps {
   title: string;
   slug: string;
